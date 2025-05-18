5ca965776d49377d4eae34dc4df8f6ce75176da6
Rohith K <rohithkalianpur@gmail.com>
2025-05-18 11:18:48 +0530

    Update energy_app.py

--- Desktop/energy-efficiency-main/energy_app.py
+++ Desktop/energy-efficiency-main/energy_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from streamlit_extras.metric_cards import style_metric_cards
+# from streamlit_extras.metric_cards import style_metric_cards
 import requests
 import json
 import math
